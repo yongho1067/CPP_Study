a2bb786abefeaa7fc1d113333d0d5727be1b6ef4
yongho1067 <dydgh960@naver.com>
2023-09-11 03:01:39 +0900

    0911

---
+++ CPP_Server/Atomic/Atomic/Atomic.cpp
@@ -0,0 +1,48 @@
+﻿#include "CorePch.h"
+#include <thread>
+#include <iostream>
+#include <atomic>
+
+// atomic atom(원자) : All - Or Nothing
+
+// DB
+// A라는 유저 인벤에서 집행검 빼고
+// B라는 유저 인벤에 집행검을 추가
+
+// 공유 데이터의 경우 쓰레드에서는 정보처리의 우선순위에 문제가 생겨 일정한 값이 안생길수도 있음
+// 그런 경우 atomic을 사용하여 해결 가능
+atomic<int32> sum = 0;
+
+void Add()
+{
+	// int sum = 0; // Stack 메모리
+	for (int32 i = 0; i < 1'000'000; i++)
+	{
+		sum.fetch_add(1);
+
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 1'000'000; i++)
+	{
+		sum.fetch_add(-1);
+	}
+}
+
+int main()
+{
+	Add();
+	Sub();
+	cout << sum << endl;
+
+	thread t1(Add);
+	thread t2(Sub);
+
+	t1.join();
+	t2.join();
+
+	cout << sum << endl;
+
+}
---
+++ CPP_Server/쓰레드 생성/쓰레드 생성/쓰레드 생성.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+#include "CorePch.h"
+
+#include <thread>
+
+void HelloThread()
+{
+	cout << "Hello Thread" << endl;
+}
+
+void HelloThread_2(int32 num)
+{
+	cout << num << endl;
+}
+
+int main()
+{
+	// System Call (OS 커널 요청)
+	cout << "Hello World" << endl;
+
+	vector<thread> v;
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		v.push_back(thread(HelloThread_2, i));
+	}
+	
+	for (int32 i = 0; i < 10; i++)
+	{
+		if (v[i].joinable())
+			v[i].join();
+	}
+
+	thread t;
+	//t = thread(HelloThread_2, 10);
+	auto id1 = t.get_id(); // 쓰레드마다 부여되는 id
+
+	int32 count = t.hardware_concurrency(); // cpu 코어 갯수?
+	auto id2 = t.get_id(); // 쓰레드마다 부여되는 id
+
+	cout << "Hello Main" << endl;
+
+
+	//t.detach(); // 쓰레드 객체에서 실제 쓰레드를 분리, thread t 는 더이상 사용 불가능해짐
+
+	if(t.joinable()) // 쓰레드가 있는지 없는지
+		t.join(); // 스레드가 종료되기 전까지 main을 종료하지 말아주세요
+	
+} 
